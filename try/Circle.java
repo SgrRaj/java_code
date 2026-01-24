a50b8087064e940c6d2b0d16598392108d5e4721
Sagar Raj <sgrraj96@gmail.com>
2026-01-24 16:19:28 +0530

    Cirle

---
+++ try/Circle.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Circle{
+
+    public static void Circumference(int r){
+        double cir=2*3.14*r;
+        System.out.println(cir);
+        return ;
+    }
+public static void main(String[] args){
+    Scanner sc=new Scanner(System.in);
+    int r=sc.nextInt();
+    Circumference(r);
+//     double result=Circumference(r);
+// System.out.println(result);
+}
+
+}
