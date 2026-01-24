de743ae4d0fed905905f2953a84b9df3db0f2157
Sagar Raj <sgrraj96@gmail.com>
2026-01-24 09:31:06 +0530

    IntLiterals

---
+++ Literals/IntLiterals.java
@@ -0,0 +1,20 @@
+class IntLiterals{ 
+public static void main(String str[]) { 
+byte b= 65; 
+System.out.println(b); 
+short s = 1234; 
+System.out.println(s); 
+int i = 123431; 
+System.out.println(i); 
+}  
+}
+
+
+
+// output is   65
+//             1234
+//             123431
+
+
+
+// 42L
