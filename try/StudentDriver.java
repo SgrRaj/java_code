49d7cc3d3aafb593a3b52bfbedf5ba42b98ae453
Sagar Raj <sgrraj96@gmail.com>
2026-01-30 23:42:59 +0530

    all

---
+++ try/StudentDriver.java
@@ -0,0 +1,19 @@
+class StudentDriver{
+	public static void main(String[] args){
+		Student s1=new Student();
+		
+		s1.name="Sagar Raj";
+		s1.age=21;
+		s1.weight=45;
+
+		
+		System.out.println(s1);
+		System.out.println(s1.name);
+		System.out.println(s1.age);
+		System.out.println(s1.weight);
+
+
+
+
+}
+}
