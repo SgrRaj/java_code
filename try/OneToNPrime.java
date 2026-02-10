9aa70f3bd56bebb0b29d8663510343176057a1b8
Sagar Raj <sgrraj96@gmail.com>
2026-02-10 20:07:29 +0530

    l

---
+++ try/OneToNPrime.java
@@ -0,0 +1,21 @@
+class OneToNPrime{
+    public static void totolPrime(int n){
+            if(n<=1){
+                System.out.println(1)
+            }
+        
+            for(int i=2;i<=n/2;i++){
+                if(n%2==0){
+                    
+                }
+            }
+            return true;
+    }
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt;
+
+        
+
+    }
+}
