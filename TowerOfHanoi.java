48de9f8ff0e6122e867a14e8f9e68814bc44dc11
Sagar Raj <sgrraj96@gmail.com>
2026-04-04 22:31:14 +0530

    up

--- TowerOfHanoi.java
+++ TowerOfHanoi.java
@@ -3,7 +3,7 @@ class TowerOfHanoi {
     
     public static void solveHanoi(int n, char source, char helper, char destination) {
         
-       
+       //
         if (n == 1) {
             System.out.println("Move disk 1 from " + source + " to " + destination);
             return;
