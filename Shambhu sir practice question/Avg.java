82a4831e3bbe40a9fe94ce3fc62fc208d461d517
Sagar Raj <sgrraj96@gmail.com>
2026-04-07 23:42:36 +0530

    up

--- Shambhu sir practice question/Avg.java
+++ Shambhu sir practice question/Avg.java
@@ -3,7 +3,7 @@ public static void Average(int a,int b,int c){
     double avg=(a+b+c)/3;
     System.out.println("average of three number:"+avg);
 }
-
+//
     public static void main(){
         int x=10;
         int y=20;
