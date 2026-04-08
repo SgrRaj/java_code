7094e10cabfa278afaefa9b16196f236c6d17f2f
Sagar Raj <sgrraj96@gmail.com>
2026-04-08 23:35:46 +0530

    isPrime

---
+++ Shambhu sir practice question/isPrimee.java
@@ -0,0 +1,27 @@
+
+
+class isPrimee{
+
+
+public static boolean isPrimee(int n){
+    if(n<2)
+    return false;
+
+    for(int i=2;i*i<=n;i++){
+        if(n%2==0){
+            return false;
+        }
+    }
+    return true;
+}
+
+
+public static void main(String[] args){
+    int n=10;
+    if(isPrimee(n)){
+        System.out.println("it is prime number");
+    }
+    else 
+     System.out.println("it is not prime number");
+}
+}
