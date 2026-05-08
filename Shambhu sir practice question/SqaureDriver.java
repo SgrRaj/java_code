6b9eb18bc90906152380e9c4651205291c8fcc99
Sagar Raj <sgrraj96@gmail.com>
2026-05-08 23:52:30 +0530

    s

--- Shambhu sir practice question/SqaureDriver.java
+++ Shambhu sir practice question/SqaureDriver.java
@@ -3,7 +3,9 @@ class SquareDriver{
         square s1=new square();
         s1.l=10;
         s1.w=5;
-        System.out.println(getDetails(s1.l,s1.w));
+        s1.color="red"; 
+
+        System.out.println(getDetails(s1.l,s1.w,s1.color));
         System.out.println(getArea(s1.l,s1.w));
         System.out.println(getPerimeter(s1.l,s1.w));
     }
