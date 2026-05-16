4b304fba4426cf2338f021d8aa068f690eda83b5
Sagar Raj <sgrraj96@gmail.com>
2026-05-16 23:57:23 +0530

    #

--- Shambhu sir practice question/Patt.java
+++ Shambhu sir practice question/Patt.java
@@ -11,7 +11,7 @@ public class Patt {
 
             
             for (int j = 1; j <= 2 * i - 1; j++) {
-                System.out.print("*");
+                System.out.print("#");
             }
 
             System.out.println();
