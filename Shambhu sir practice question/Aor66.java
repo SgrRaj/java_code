a4fbe6bbf5aab4c0a161f62fd83627f470611d58
Sagar Raj <sgrraj96@gmail.com>
2026-05-26 22:28:08 +0530

    Ascii value updated

---
+++ Shambhu sir practice question/Aor66.java
@@ -0,0 +1,7 @@
+class Aor66{
+    public static void main(String[] args) {
+      System.out.println("A" + 1);  //Output: A1 (because "A" is a string, so "A" + 1 results in string concatenation)
+
+        System.out.println('A' + 1); //Output: 66 (because 'A' is 65 in ASCII, so 'A' + 1 = 65 + 1 = 66)
+    }
+}
