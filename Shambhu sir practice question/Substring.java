889fc716564de2b9b0da3fae5b667939f410926e
Sagar Raj <sgrraj96@gmail.com>
2026-06-02 23:08:12 +0530

    SubString

---
+++ Shambhu sir practice question/Substring.java
@@ -0,0 +1,16 @@
+
+
+public class Substring {
+
+    public static void main(String[] args)
+    {
+
+        String s = "Welcome to Java Programming";
+
+       
+        String sub = s.substring(8);
+
+      
+        System.out.println("Substring: " + sub);
+    }
+}
