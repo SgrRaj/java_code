4c9039a451b07158c411b4dc413fb0886586e05e
Sagar Raj <sgrraj96@gmail.com>
2026-06-03 14:42:56 +0530

    AllUniqueSubString

---
+++ Shambhu sir practice question/AllUniqueSubstring.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class AllUniqueSubstring {
+    public static void main(String[] args) {
+        String s= "abaccbd";
+         printAllUniqueSubstring(s);
+  
+    }
+
+    public static void printAllUniqueSubstring(String s){
+      Map<Character,Integer> m = new HashMap<>();
+      Set<String>  uniqueSubstrings = new HashSet<>();
+      int left=0 ,right=0;
+      while(right<s.length()){
+        char c = s.charAt(right);
+      if(m.getOrDefault(c,-1)+1>left){
+        left = m.getOrDefault(c,-1)+1;
+      }
+
+      uniqueSubstrings.add(s.substring(left,right+1));
+      m.put(c,right);
+      right++;
+    }
+
+    System.out.println("All unique substrings are: "+ uniqueSubstrings.size());
+    for(String substring :  uniqueSubstrings){
+        System.out.println(substring);
+    }
+}
+}
