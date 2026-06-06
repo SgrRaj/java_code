3239d05ad1730194c8c4d4288e9e5f13f406d49d
Sagar Raj <sgrraj96@gmail.com>
2026-06-06 21:47:11 +0530

    DP print 1 to 10

---
+++ DP/PrintNaturalNumber.java
@@ -0,0 +1,15 @@
+public class PrintNaturalNumber {
+    public static void main(String[] args) {
+        printNum(1);
+    }
+    public static void printNum(int n){
+        if(n>10){
+            System.out.println("=========");
+            return;
+        }
+        System.out.println(n);
+        printNum(n+1);
+        // System.out.println(n);
+        
+    }
+}
