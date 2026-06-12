52734773f8b4af0a1f653812df081d35b3275dfd
Sagar Raj <sgrraj96@gmail.com>
2026-06-12 23:42:46 +0530

    Path trim

---
+++ String/Path17.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class Path17{
+public static void main(String[] args){
+Scanner sc =new Scanner(System.in); 
+System.out.println("Enter a String ");
+String s=sc.nextLine(); 
+String res=path(s);
+System.out.println(res);
+
+}
+
+public static String path(String s){
+    String s1="";
+for(int i=s.length()-1;i>=0;i--){
+    if(s.charAt(i)!='\\'){
+        s1=s.charAt(i)+s1;
+        break;
+        
+    }
+}
+return s1;
+}
+}
